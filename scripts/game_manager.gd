d1e57e5695aeefde5f10b8142eda2fe2861541de
pauytrh2 <pauytrh@gmail.com>
2025-07-31 15:11:03 +0300

    Transform enemy spawner into game manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,8 +8,13 @@ func _ready() -> void:
 func _on_enemy_killed() -> void:
     spawn_enemy()
 
+func _on_area_entered(area: Area2D) -> void:
+    print(area.name, ": ", area.is_in_group("player"))
+    
+
 func spawn_enemy() -> void:
     var enemy_instance = enemy_scene.instantiate()
     enemy_instance.add_to_group("enemies")
     enemy_instance.killed.connect(_on_enemy_killed)
+    enemy_instance.area_entered.connect(_on_area_entered)
     add_child(enemy_instance)
