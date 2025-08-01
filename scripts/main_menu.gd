724a99b10e3cddadb9745f339f39c2cf624bb74a
pauytrh2 <pauytrh@gmail.com>
2025-08-01 20:31:03 +0300

    Update main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var settings: Panel = $Settings
+@onready var main_buttons: VBoxContainer = $MainMenu/MainButtons
 
 func _on_start_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_settings_pressed() -> void:
-    pass # Replace with function body.
+    main_buttons.hide()
+    settings.show()
 
 func _on_quit_pressed() -> void:
     get_tree().quit()
+
+func _on_back_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
