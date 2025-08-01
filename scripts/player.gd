a0241da4fd642cc6e87fee5ce13b8b214f6bda54
pauytrh2 <pauytrh@gmail.com>
2025-08-01 14:26:00 +0300

    Add zoom

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,9 @@ extends CharacterBody2D
 
 var facing_left := false
 var is_slashing := false
+const max_zoom := Vector2(1.1, 1.1)
+const min_zoom := Vector2(0.9, 0.9)
+const zoom_threshold := 200.0
 
 const ATTACK_OFFSET := 14
 
@@ -16,6 +19,8 @@ func _physics_process(delta) -> void:
         return
     if Input.is_action_just_pressed("slash"):
         slash()
+
+    zoom(delta)
 
     movement(delta)
 
@@ -63,3 +68,18 @@ func movement(delta) -> void:
     rotation = normal.angle() + PI / 2
     var tangent = Vector2(normal.y, -normal.x) if facing_left else Vector2(-normal.y, normal.x)
     global_position += tangent * Globals.player_speed * delta
+
+func zoom(delta) -> void:
+    var t: float = clamp(get_closet_enemy_dist() / zoom_threshold, 0.0, 1.0)
+    var desired_zoom = min_zoom.lerp(max_zoom, 1.0 - pow(t, 2))
+    $Camera2D.zoom = $Camera2D.zoom.lerp(desired_zoom, delta * 5.0)
+
+func get_closet_enemy_dist() -> float:
+    var closest_enemy_dist := INF
+
+    for enemy in get_tree().get_nodes_in_group("enemies"):
+        var dist = global_position.distance_to(enemy.global_position)
+        if dist < closest_enemy_dist:
+            closest_enemy_dist = dist
+
+    return closest_enemy_dist
