3b2969fff9e55a7bc93652a0650871eb13856ca3
pauytrh2 <pauytrh@gmail.com>
2025-08-03 15:52:31 +0300

    Add click sound effect

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,20 +2,28 @@ extends Control
 
 @onready var settings: Panel = $Settings
 @onready var main_buttons: VBoxContainer = $MainMenu/MainButtons
+@onready var sfx_player: AudioStreamPlayer = $SFXPlayer
 
 func _ready() -> void:
     main_buttons.show()
     settings.hide()
 
 func _on_start_pressed() -> void:
+    play_sound()
     get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_settings_pressed() -> void:
+    play_sound()
     main_buttons.hide()
     settings.show()
 
 func _on_quit_pressed() -> void:
+    play_sound()
     get_tree().quit()
 
 func _on_back_pressed() -> void:
+    play_sound()
     _ready()
+
+func play_sound() -> void:
+    sfx_player.play()
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,19 +1,26 @@
 extends Node
 
 @onready var pause_menu: Panel = %PauseMenu
+@onready var sfx_player: AudioStreamPlayer = $"../SFXPlayer"
 
 func _ready() -> void:
     pause_menu.hide()
     get_tree().paused = false
 
 func _on_back_pressed() -> void:
+    play_sound()
     pause_menu.hide()
     get_tree().paused = false
 
 func _on_main_menu_pressed() -> void:
+    play_sound()
     get_tree().paused = false
     get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_pause_pressed() -> void:
+    play_sound()
     pause_menu.show()
     get_tree().paused = true
+
+func play_sound() -> void:
+    sfx_player.play()
